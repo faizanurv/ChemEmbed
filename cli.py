d75da9980b6c4738dfca6beb8f7b4e099f6e4dd1
Neil Butcher <n.a.butcher@bham.ac.uk>
2026-08-12 09:50:31 +0100

    consolidating 2 main & cli into one chemembed_single_file

--- cli.py
+++ cli.py
@@ -1,33 +1,7 @@
-# main.py
+# cli.py
 
-import pandas as pd
-import yaml
 import argparse
-import torch
-from torch.utils.data import DataLoader
-
-# Import functions from our modules
-from data_processing import (
-    msp_to_dataframe_with_smiles,
-    msp_to_dataframe_without_smiles,
-    preprocess_spectra_with_smiles,
-    preprocess_spectra_without_smiles,
-    process_data_with_smiles,
-    process_data_without_smiles
-)
-
-from model_utils import (
-    load_model,
-    predict_with_smiles,
-    predict_without_smiles
-)
-
-from reference_utils import (
-    load_reference_database_with_smiles,
-    load_reference_database_without_smiles,
-    match_predictions_to_reference_with_smiles,
-    match_predictions_to_reference_without_smiles
-)
+from chemembed_single_file import run
 
 
 def main():
@@ -61,63 +35,8 @@ def main():
 
     args = parser.parse_args()
 
-    input_type = args.input_file_type
-    adduct = args.adduct
+    run(vars(args))
 
-    if input_type == 'with_smiles':
-        # Processing for 'with_smiles'
-        if args.adduct == "+":
-            print("Positive Point")
-            msp_df = msp_to_dataframe_with_smiles(args.msp_file_positive)
-        else:
-            print("Negative Point")
-            msp_df = msp_to_dataframe_with_smiles(args.msp_file_negative)
-        norm_df = preprocess_spectra_with_smiles(msp_df, args.intensity_threshold)
-        final_up = process_data_with_smiles(norm_df, args.tolerance, args.resolution, args.max_mz)
-        final_up.to_pickle(args.preprocessed_data)
-        from data_loaders import inference_dataset_loader as data_loader_module
-        test_dataset = data_loader_module.class_ls(args.preprocessed_data)
-        predict_fn = predict_with_smiles
-        reference_loader_fn = load_reference_database_with_smiles
-        matcher_fn = match_predictions_to_reference_with_smiles
-    else:
-        # Processing for 'without_smiles'
-        if args.adduct == "+":
-            msp_df = msp_to_dataframe_without_smiles(args.msp_file_positive)
-        else:
-            msp_df = msp_to_dataframe_without_smiles(args.msp_file_negative)
-        norm_df = preprocess_spectra_without_smiles(msp_df, args.intensity_threshold)
-        final_up = process_data_without_smiles(norm_df, args.tolerance, args.resolution, args.max_mz)
-        final_up.to_pickle(args.preprocessed_data)
-        from data_loaders import spectra_inference_dataset_loader as data_loader_module
-        test_dataset = data_loader_module.class_ls(args.preprocessed_data)
-        predict_fn = predict_without_smiles
-        reference_loader_fn = load_reference_database_without_smiles
-        matcher_fn = match_predictions_to_reference_without_smiles
-
-    test_loader = DataLoader(dataset=test_dataset,
-                             batch_size=1,
-                             drop_last=True,
-                             shuffle=False,
-                             num_workers=0)
-    if args.adduct == "+":
-        #print("Positive model load")
-        model_cnn = load_model(args.model_path_positive)
-    else:
-        #print("Negative model load")
-        model_cnn = load_model(args.model_path_negative)
-
-    # Make predictions
-    prediction_df = predict_fn(model_cnn, test_loader, input_type)
-
-    # Load and preprocess reference database
-    reference_df = reference_loader_fn(args.reference_database, adduct)
-
-    # Match predictions to reference database
-    final_results_df = matcher_fn(prediction_df, reference_df, args.top_n_candidates, input_type, adduct)
-
-    # Save final results
-    final_results_df.to_csv(args.prediction_results, index=False)
 
 if __name__ == "__main__":
     main()
