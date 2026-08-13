63ea6baf9c2830eb110e54c6e474b4aef490c9e4
Neil Butcher <n.a.butcher@bham.ac.uk>
2026-08-13 11:40:49 +0100

    removed the requirements of always having both + and - files

--- cli.py
+++ cli.py
@@ -14,12 +14,12 @@ def main():
     parser.add_argument('--adduct', type=str, default='+', choices=["+", "-"], help='Choose "-" for M-H or "+" for M+H based on the adduct type.')
 
     # input files
-    parser.add_argument('--msp_file_positive', type=str, required=True, help='Path to the positive mode spectra file.')
-    parser.add_argument('--msp_file_negative', type=str, required=True, help='Path to the negative mode spectra file.')
+    parser.add_argument('--msp_file_positive', type=str, help='Path to the positive mode spectra file.')
+    parser.add_argument('--msp_file_negative', type=str, help='Path to the negative mode spectra file.')
 
     # models
-    parser.add_argument('--model_path_positive', type=str, required=True, help='Path to the trained positive mode model.')
-    parser.add_argument('--model_path_negative', type=str, required=True, help='Path to the trained negative mode model.')
+    parser.add_argument('--model_path_positive', type=str, help='Path to the trained positive mode model.')
+    parser.add_argument('--model_path_negative', type=str, help='Path to the trained negative mode model.')
     parser.add_argument('--reference_database', type=str, required=True, help='Path to your reference database as pkl file.')
 
     # parameters
@@ -34,6 +34,19 @@ def main():
     parser.add_argument('--prediction_results', type=str, default='prediction_results.csv', help='Path to save prediction results as csv file.')
 
     args = parser.parse_args()
+
+    if args.adduct == "+" and not args.msp_file_positive:
+        parser.error("--msp_file_positive is required when --adduct is '+'")
+
+    if args.adduct == "-" and not args.msp_file_negative:
+        parser.error("--msp_file_negative is required when --adduct is '-'")
+
+    if args.adduct == "+" and not args.model_path_positive:
+        parser.error("--model_path_positive is required when --adduct is '+'")
+
+    if args.adduct == "-" and not args.model_path_negative:
+        parser.error("--model_path_negative is required when --adduct is '-'")
+
 
     run(vars(args))
 
